61c29216ca9d5b3bf83469ef30195d8540f73baf
vidya <vzg805@gmail.com>
2023-09-05 21:45:51 -0500

    add border and grid patter

--- iteration1/sketch.js
+++ iteration1/sketch.js
@@ -6,10 +6,10 @@ function setup() {
   var canvas = createCanvas(0.98 * windowHeight, 0.98 * windowHeight);
   background(200, 200, 200);
   rectMode(RADIUS);
+  ellipseMode(RADIUS);
 }
 
 // TO DO:
-// - write border pattern function
 // - write grid pattern function and make it apply for IsInner
 // - add randomness in colors and sizes
 
@@ -24,16 +24,66 @@ function draw() {
   generatedLayer([255, 255, 0], width / 2.5, 7);
 
   // inner layer
-  generatedLayer([255, 100, 0], width / 3, 4.5);
+  generatedLayer([255, 100, 0], width / 3, 4.5, true);
 }
 
 function cornerPattern(cornerParam) {
   noStroke();
   fill(0, 255, 0);
-  ellipse(width / cornerParam, width / cornerParam, 20, 20);
-  ellipse(width - width / cornerParam, width / cornerParam, 20, 20);
-  ellipse(width / cornerParam, width - width / cornerParam, 20, 20);
-  ellipse(width - width / cornerParam, width - width / cornerParam, 20, 20);
+  var cornerSize = 10;
+  ellipse(width / cornerParam, width / cornerParam, cornerSize, cornerSize);
+  ellipse(width - width / cornerParam, width / cornerParam, cornerSize, cornerSize);
+  ellipse(width / cornerParam, width - width / cornerParam, cornerSize, cornerSize);
+  ellipse(width - width / cornerParam, width - width / cornerParam, cornerSize, cornerSize);
+}
+
+function borderPattern(cornerParam) {
+  noStroke();
+  fill(0, 255, 0);
+  var borderlength = width - 2 * (width / cornerParam);
+  var dotSize = 5;
+
+  var spacing = borderlength / (2 * dotSize);
+  // top
+  for (let i = 0; i < spacing; i++) {
+    ellipse(width / cornerParam + i * 2 * dotSize, width / cornerParam, dotSize / 2, dotSize / 2);
+  }
+
+  // bottom
+  for (let i = 0; i < spacing; i++) {
+    ellipse(width / cornerParam + i * 2 * dotSize, width - width / cornerParam, dotSize / 2, dotSize / 2);
+  }
+
+  // right
+  for (let i = 0; i < spacing; i++) {
+    ellipse(width - width / cornerParam, width / cornerParam + i * 2 * dotSize, dotSize / 2, dotSize / 2);
+  }
+
+  // left
+  for (let i = 0; i < spacing; i++) {
+    ellipse(width / cornerParam, width / cornerParam + i * 2 * dotSize, dotSize / 2, dotSize / 2);
+  }
+}
+
+function gridPattern(cornerParam) {
+  noStroke();
+  fill(0, 0, 200);
+  var gridPadding = 20;
+  var borderlength = width - 2 * (width / cornerParam) - 2 * gridPadding;
+  var dotSize = 5;
+
+  var spacing = borderlength / (2 * dotSize);
+  // top
+  for (let i = 0; i < spacing; i++) {
+    for (let j = 0; j < spacing; j++) {
+      ellipse(
+        gridPadding + width / cornerParam + i * 2 * dotSize,
+        gridPadding + width / cornerParam + j * 2 * dotSize,
+        dotSize / 2,
+        dotSize / 2
+      );
+    }
+  }
 }
 
 function generatedLayer(layerColor, layerSize, cornerParam, isInner) {
@@ -41,4 +91,8 @@ function generatedLayer(layerColor, layerSize, cornerParam, isInner) {
   noStroke();
   rect(width / 2, height / 2, layerSize, layerSize);
   cornerPattern(cornerParam);
+  borderPattern(cornerParam);
+  if (isInner) {
+    gridPattern(cornerParam);
+  }
 }
